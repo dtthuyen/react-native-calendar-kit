d81ed333e277d4244132cc7ae1b66387abaa97dc
dtthuyen <dtth247vp@gmail.com>
2023-09-28 09:09:09 +0700

    add theme

---
+++ src/theme/types.d.ts
@@ -0,0 +1,101 @@
+import "styled-components/native";
+
+declare module 'styled-components/native' {
+  export interface DefaultTheme {
+    name: string;
+    primaryColor: string;
+    backgroundHeader: string;
+    underlayColor: string;
+    backgroundColor: string;
+    secondaryBackgroundColor: string;
+    grey1: string;
+    grey2: string;
+    grey3: string;
+    grey4: string;
+    grey5: string;
+    grey6: string;
+    grey7: string;
+    divider: string;
+    white: string;
+    black: string;
+    black10: string;
+    error: string;
+    success: string;
+    warn: string;
+    disabled: string,
+    active: string,
+    late: string
+    blue1: string;
+    blue2: string;
+    blue3: string;
+    green1: string;
+    green2: string;
+    green3: string;
+    green4: string;
+    green5: string;
+    green6: string;
+    red1: string;
+    red2: string;
+    red3: string;
+    red4: string;
+    red5: string;
+    red6: string;
+    orange: string;
+    cardBackground: string;
+    card: string;
+    today: string;
+    dayText: string;
+    attendees: string,
+    floatingButton: string;
+  }
+}
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    name: string;
+    primaryColor: string;
+    backgroundHeader: string;
+    underlayColor: string;
+    backgroundColor: string;
+    secondaryBackgroundColor: string;
+    grey1: string;
+    grey2: string;
+    grey3: string;
+    grey4: string;
+    grey5: string;
+    grey6: string;
+    grey7: string;
+    divider: string;
+    white: string;
+    black: string;
+    black10: string;
+    error: string;
+    success: string;
+    warn: string;
+    disabled: string,
+    active: string,
+    late: string
+    blue1: string;
+    blue2: string;
+    blue3: string;
+    green1: string;
+    green2: string;
+    green3: string;
+    green4: string;
+    green5: string;
+    green6: string;
+    red1: string;
+    red2: string;
+    red3: string;
+    red4: string;
+    red5: string;
+    red6: string;
+    orange: string;
+    cardBackground: string;
+    card: string;
+    today: string;
+    dayText: string;
+    attendees: string,
+    floatingButton: string;
+  }
+}
